3f713142e570954fb4aaaa8e06a7018acace3b18
leaneleo <leaneleofit@gmail.com>
2025-04-16 21:25:09 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   function revelarElementos() {
     const reveals = document.querySelectorAll(".reveal");
-    for (let i = 0; i < reveals.length; i++) {
+    reveals.forEach(el => {
       const windowHeight = window.innerHeight;
-      const elementTop = reveals[i].getBoundingClientRect().top;
-      const elementVisible = 100;
+      const elementTop = el.getBoundingClientRect().top;
+      const elementVisible = 150;
       if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("ativo");
+        el.classList.add("ativo");
       } else {
-        reveals[i].classList.remove("ativo");
+        el.classList.remove("ativo");
       }
-    }
+    });
   }
 
   window.addEventListener("scroll", revelarElementos);
