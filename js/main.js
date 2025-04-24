9eda831658d78d5e3ffd0fe9933285cc4947e46d
leaneleo <leaneleofit@gmail.com>
2025-04-24 14:13:15 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (elementTop < windowHeight - elementVisible) {
         elemento.classList.add("ativo");
+      } else {
         elemento.classList.remove("ativo");
       }
     });
@@ -23,43 +24,4 @@ const menu = document.getElementById('menu');
 
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('active');
-});
-
-const track = document.getElementById('carrosselTrack');
-
-let isDown = false;
-let startX;
-let scrollLeft;
-
-track.addEventListener('mousedown', (e) => {
-  isDown = true;
-  track.classList.add('pause');
-  startX = e.pageX - track.offsetLeft;
-  scrollLeft = track.scrollLeft;
-});
-
-track.addEventListener('mouseleave', () => {
-  isDown = false;
-  track.classList.remove('pause');
-});
-
-track.addEventListener('mouseup', () => {
-  isDown = false;
-  track.classList.remove('pause');
-});
-
-track.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - track.offsetLeft;
-  const walk = (x - startX) * 1.5;
-  track.scrollLeft = scrollLeft - walk;
-});
-
-track.addEventListener('touchstart', () => {
-  track.classList.add('pause');
-});
-
-track.addEventListener('touchend', () => {
-  track.classList.remove('pause');
 });
